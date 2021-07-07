608d7ac6bf0b1805ced8c71a85bdbecae3b195a6
samocite <543604007@qq.com>
2021-07-08 03:32:17 +0800

    2021.7.8_03:32

--- f.h
+++ f.h
@@ -4,17 +4,19 @@
 #include <string>
 using namespace std;
 
-string user_name;	//变量声明
+//变量声明
+string user_name;
 string user_server;
 string user_passwd;
 string user_db;
-int  usr_port;
+int usr_port;
 string databases;
 
-int remake(void);    //函数声明
-void connect(MYSQL *now_sql);
-int mysql_now(MYSQL *now_sql);
+//函数声明
+int remake(void);//初始化内部
+int connect(MYSQL *now_sql);//连接
+int mysql_now(MYSQL *now_sql);//初始化数据库
 int insertData(MYSQL *now_sql);  //增
-int  deleteName(MYSQL *now_sql); //删
-int  updateByName(MYSQL *now_sql);  //改，也可以理解为更新字段，需要指定 
-void quit(MYSQL * now_sql);
+int deleteName(MYSQL *now_sql); //删
+int updateByName(MYSQL *now_sql);  //改，也可以理解为更新字段，需要指定
+void quit(MYSQL * now_sql);//退出数据库
--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include "f.h"
 
 using namespace std;
+/*
+void Display_Menu() {//展示菜单
 
-MYSQL *now_sql;
+}
+int (*Choose_Menu())(MYSQL* DataBase) {//选择操作
 
-int main() {
+}
+void RunCode() {//执行操作
 
-    if (mysql_now(now_sql)) {
-        if (connect(now_sql));
-
+}
+*/
+MYSQL* Now_DataBase;//当前操作数据库
+int main(int argc, char** argv) {
+    if (mysql_now(Now_DataBase) && connect(Now_DataBase)) {//初始化并连接
+        //Display_Menu();
+        //RunCode(Choose_Menu());
     }
-
     else {
         return 0;
-        p
     }
 }
